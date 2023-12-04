eda7c79c1211e98dd683576bc41575028541b6d2
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2023-12-03 23:36:53 -0500

    Animation playing

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
-const present = "bg-gray-800";
+const present: string[] = [];
 export function Model() {
   useEffect(() => {
     const iobs = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add(present);
+            entry.target.classList.add(...present);
           } else {
-            entry.target.classList.remove(present);
+            entry.target.classList.remove(...present);
           }
         });
       },
@@ -25,9 +28,28 @@ export function Model() {
       iobs.disconnect();
     };
   }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center fixed -z-10">
-      <h1 className="text-6xl font-bold">Model</h1>
+    <div className="w-screen h-screen -z-10 fixed">
+      <Canvas
+        camera={{ position: [0, 0, -5] }}
+      >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[5, 5, -5]} decay={0.25} />
+        <Suspense fallback={null}>
+          <Plague />
+        </Suspense>
+      </Canvas>
     </div>
   );
+}
+
+function Plague() {
+  const { scene, animations } = useGLTF("/plague.glb");
+  const mixer = new THREE.AnimationMixer(scene);
+  mixer.clipAction(animations[0]).play();
+  useFrame((state,delta) => {
+    mixer.update(delta);
+  })
+  return <primitive object={scene} />;
 }
