c7d89c3890e61e19176cf9d50ea2cef172907be6
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2024-02-23 18:35:00 -0500

    Projects card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+// @ts-check
 import animate from "tailwindcss-animate";
 import catppuccin from "@catppuccin/tailwindcss";
+import flowbite from "flowbite/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
@@ -13,5 +18,6 @@ export default {
       prefix: "cat",
       defaultFlavour: "mocha",
     }),
+    flowbite,
   ],
 };
