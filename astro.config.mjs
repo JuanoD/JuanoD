7d62ad12b0f616bdeb77506825f1356ce5cf3308
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2025-09-19 20:45:28 -0500

    tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,33 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
+import tailwindcss from "@tailwindcss/vite";
 
 const site = "https://juanod.com";
 
 // https://astro.build/config
 export default defineConfig({
-  // vite: {
-  //   resolve: {
-  //     alias: {
-  //       three: resolve("./src/three/exports.js"),
-  //       three_modules: resolve("./node_modules/three"),
-  //     },
-  //   },
-  // },
   site,
+
   image: {
     domains: ["skillicons.dev", "raw.githubusercontent.com"],
   },
+
   integrations: [
-    tailwind(),
     react({
       experimentalReactChildren: true,
     }),
     robotsTxt({ sitemap: false, host: true }),
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+    // resolve: {
+    //   alias: {
+    //     three: resolve("./src/three/exports.js"),
+    //     three_modules: resolve("./node_modules/three"),
+    //   },
+    // },
+  },
 });
